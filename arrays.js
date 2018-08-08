46726789aa9cfe675ea5634127c6cd9b1b648077
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-08 14:49:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
 function addElementToBeginningOfArray(array, element){
   return chocolateBars.unshift 
 }
+
+function addElem
