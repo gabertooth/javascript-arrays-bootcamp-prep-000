3d71b7a8ba4dcc513c19aab464b2aa67ef1b34f4
gblatstein@gmail.com <gblatstein@gmail.com>
2018-08-08 15:49:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+function destructivelyRemoveElementToEndOfArray(array){
+  
+}
